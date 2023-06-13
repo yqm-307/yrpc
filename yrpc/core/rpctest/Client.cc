61c809ffdae056a752a2805c883215231d8e5856
yqm-307 <979336542@qq.com>
2023-06-13 22:40:35 +0800

    修改文件夹名称

--- yrpc/core/rpctest/Client.cc
+++ yrpc/core/rpctest/Client.cc
@@ -1,67 +1,67 @@
-#include "../YRpc.h"
-#include "../../proto/test_protocol/AddAndStr.pb.h"
-#include <memory>
-#include <bbt/timer/interval.hpp>
-#include <bbt/config/GlobalConfig.hpp>
-using namespace yrpc;
-using namespace yrpc::util;
-using namespace yrpc::util::clock;
+// #include "../YRpc.h"
+// #include "../../proto/test_protocol/AddAndStr.pb.h"
+// #include <memory>
+// #include <bbt/timer/interval.hpp>
+// #include <bbt/config/GlobalConfig.hpp>
+// using namespace yrpc;
+// using namespace yrpc::util;
+// using namespace yrpc::util::clock;
 
-typedef google::protobuf::Message Message;
-typedef std::shared_ptr<Message> MessagePtr;
+// typedef google::protobuf::Message Message;
+// typedef std::shared_ptr<Message> MessagePtr;
 
-std::atomic_int ccount{0};
-yrpc::detail::net::YAddress addr("127.0.0.1",12020);
-// int ccount=0;
+// std::atomic_int ccount{0};
+// yrpc::detail::net::YAddress addr("127.0.0.1",12020);
+// // int ccount=0;
 
-void call_once(const std::string& once )
-{
-    auto pck = EchoReq();
-    pck.set_str(once);    
-    auto co = rpc::CallObjFactory::GetInstance()->Create<EchoReq,EchoRsp>(
-        std::move(pck),"Echo",
-        [](std::shared_ptr<google::protobuf::Message> rsp){
-            // ccount.fetch_add(1,std::memory_order_seq_cst);
-            ++ccount;
-            // printf("完成了第%d次\n",ccount.load());
-        });
-    assert(co);
-    while (rpc::Rpc::RemoteOnce(addr,"Echo",co) < 0);
-}
+// void call_once(const std::string& once )
+// {
+//     auto pck = EchoReq();
+//     pck.set_str(once);    
+//     auto co = rpc::CallObjFactory::GetInstance()->Create<EchoReq,EchoRsp>(
+//         std::move(pck),"Echo",
+//         [](std::shared_ptr<google::protobuf::Message> rsp){
+//             // ccount.fetch_add(1,std::memory_order_seq_cst);
+//             ++ccount;
+//             // printf("完成了第%d次\n",ccount.load());
+//         });
+//     assert(co);
+//     while (rpc::Rpc::RemoteOnce(addr,"Echo",co) < 0);
+// }
 
-void run_for_times(const std::string& ip,int port,int ntimes=-1)
-{
-    // rpc::RpcClient client("127.0.0.1", 12020);
-    yrpc::util::lock::Sem_t cond;
-    ntimes = ntimes > 0 ? ntimes : 100000;
-    bbt::timer::interval now;
-    client.AsyncConnect([&](){
+// void run_for_times(const std::string& ip,int port,int ntimes=-1)
+// {
+//     // rpc::RpcClient client("127.0.0.1", 12020);
+//     yrpc::util::lock::Sem_t cond;
+//     ntimes = ntimes > 0 ? ntimes : 100000;
+//     bbt::timer::interval now;
+//     client.AsyncConnect([&](){
 
-        for (int i = 0;i<ntimes;++i)
-        {
-            call_once(client,"hello world");
-        }
-    });
-    while(ccount<ntimes){std::this_thread::sleep_for(std::chrono::milliseconds(5));}
-    printf("调用 over!\n");
-    printf("耗时: %ldms\n",now.intervalnow());
-}
+//         for (int i = 0;i<ntimes;++i)
+//         {
+//             call_once(client,"hello world");
+//         }
+//     });
+//     while(ccount<ntimes){std::this_thread::sleep_for(std::chrono::milliseconds(5));}
+//     printf("调用 over!\n");
+//     printf("耗时: %ldms\n",now.intervalnow());
+// }
 
-int main(int nparam,char* argv[])
-{
-    // if ( nparam != 3 )
-    // {
-    //     printf("usage : [ip] [port]");
-    //     exit(-1);
-    // }
+// int main(int nparam,char* argv[])
+// {
+//     // if ( nparam != 3 )
+//     // {
+//     //     printf("usage : [ip] [port]");
+//     //     exit(-1);
+//     // }
 
-    int flag = 1;
-    bbt::config::GlobalConfig::GetInstance()->GetDynamicCfg()->SetEntry(bbt::config::BBTSysCfg[bbt::config::BBT_LOG_STDOUT_OPEN], &flag);
-    // std::string ip(argv[1],sizeof(argv[1]));
-    // int port = std::stoi(std::string(argv[2],sizeof(argv[2])));
+//     int flag = 1;
+//     bbt::config::GlobalConfig::GetInstance()->GetDynamicCfg()->SetEntry(bbt::config::BBTSysCfg[bbt::config::BBT_LOG_STDOUT_OPEN], &flag);
+//     // std::string ip(argv[1],sizeof(argv[1]));
+//     // int port = std::stoi(std::string(argv[2],sizeof(argv[2])));
     
-    int port = 12020;
-    std::string ip("127.0.0.1");
+//     int port = 12020;
+//     std::string ip("127.0.0.1");
 
-    run_for_times(ip,port,-1);
-}
+//     run_for_times(ip,port,-1);
+// }
--- yrpc/core/rpctest/Server.cc
+++ yrpc/core/rpctest/Server.cc
@@ -1,36 +1,36 @@
-#include "../../proto/test_protocol/AddAndStr.pb.h"
-#include "../YRpc.h"
-#include <memory>
-#include <bbt/config/GlobalConfig.hpp>
-using namespace yrpc;
-using namespace yrpc::util;
-typedef std::function<void(std::shared_ptr<google::protobuf::Message>)> SendPacket;
-typedef std::shared_ptr<google::protobuf::Message> MessagePtr;
+// #include "../../proto/test_protocol/AddAndStr.pb.h"
+// #include "../YRpc.h"
+// #include <memory>
+// #include <bbt/config/GlobalConfig.hpp>
+// using namespace yrpc;
+// using namespace yrpc::util;
+// typedef std::function<void(std::shared_ptr<google::protobuf::Message>)> SendPacket;
+// typedef std::shared_ptr<google::protobuf::Message> MessagePtr;
 
 
 
-MessagePtr AddHandle(MessagePtr args)
-{
-    std::shared_ptr<AddReq> req = std::static_pointer_cast<AddReq>(args);
-    int rval =  req->a();
-    int lval = req->b();
-    auto rsp = std::make_shared<AddRsp>();
-    rsp->set_result(rval+lval);
-    return rsp;
-}
+// MessagePtr AddHandle(MessagePtr args)
+// {
+//     std::shared_ptr<AddReq> req = std::static_pointer_cast<AddReq>(args);
+//     int rval =  req->a();
+//     int lval = req->b();
+//     auto rsp = std::make_shared<AddRsp>();
+//     rsp->set_result(rval+lval);
+//     return rsp;
+// }
 
-MessagePtr EchoHandle(const MessagePtr args)
-{
-    const std::shared_ptr<EchoReq> req = std::static_pointer_cast<EchoReq>(args);
-    std::string str = req->str();
-    auto rsp = std::make_shared<EchoRsp>();
-    rsp->set_str(str);
-    return rsp;
-}
+// MessagePtr EchoHandle(const MessagePtr args)
+// {
+//     const std::shared_ptr<EchoReq> req = std::static_pointer_cast<EchoReq>(args);
+//     std::string str = req->str();
+//     auto rsp = std::make_shared<EchoRsp>();
+//     rsp->set_str(str);
+//     return rsp;
+// }
 
-int main()
-{
+// int main()
+// {
 
-    while(true)
-        std::this_thread::sleep_for(yrpc::util::clock::s(3)); 
-}
+//     while(true)
+//         std::this_thread::sleep_for(yrpc::util::clock::s(3)); 
+// }
